519224cc3bc5913cdd14174a1baa5088ad3fd6e8
Anestezia-zip <plekatybtc@gmail.com>
2023-03-29 16:16:24 +0100

    Corrected code

--- src/app/lesson04/sort.pipe.ts
+++ src/app/lesson04/sort.pipe.ts
@@ -2,17 +2,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Person } from './lesson04.component';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false
 })
+
 export class SortPipe implements PipeTransform {
+
   transform(values: Person[], sort: string): Person[] {
     if (!values) return [];
     if (!sort) return values;
     if(sort === 'asc') {
-      return values.sort((a, b) => (a.firstName > b.firstName) ? 1 : ((a.firstName < b.firstName) ? -1 : 0) )
+      return values.sort((a, b) => (a.firstName.toLowerCase() > b.firstName.toLowerCase()) ? 1 : ((a.firstName.toLowerCase() < b.firstName.toLowerCase()) ? -1 : 0) )
     }
-    return values.sort((a, b) => (a.firstName < b.firstName) ?  1 : ((a.firstName > b.firstName) ? -1 : 0))
-  }
-
+      return values.sort((a, b) => (a.firstName.toLowerCase() < b.firstName.toLowerCase()) ? 1 : ((a.firstName.toLowerCase() > b.firstName.toLowerCase()) ? -1 : 0))
+    }
+  
 }
-
